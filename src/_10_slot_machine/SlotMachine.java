25c68ab831164644bceb3ff8cd5a01151a180162
league <league@imac-24.attlocal.net>
2023-02-11 12:58:43 -0800

    Get the if statement done

--- src/_10_slot_machine/SlotMachine.java
+++ src/_10_slot_machine/SlotMachine.java
@@ -2,6 +2,8 @@ package _10_slot_machine;
 
 
 import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.net.MalformedURLException;
 import java.net.URL;
 import javax.swing.Icon;
@@ -12,25 +14,38 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 
-public class SlotMachine {
+public class SlotMachine implements ActionListener {
 	JFrame frame = new JFrame();
 	JPanel pane = new JPanel();
-	JButton spin = new JButton();
-	JLabel seven = new JLabel();
+	JButton spin = new JButton("spin");
+	JLabel slot1 = new JLabel();
+	JLabel slot2 = new JLabel();
+	JLabel slot3 = new JLabel();
+	
+	
+	
 	
 	
 	public void machine() {
 		frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
-		frame.setPreferredSize(new Dimension(1000,1000));
+		frame.setPreferredSize(new Dimension(1600,1000));
 		frame.add(pane);
+		
 		try {
-			seven = createLabelImage("video_slot_machine_symbols_7_thumbnail_square_0000.png");
-		} catch (MalformedURLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+						slot1 = createLabelImage("video_slot_machine_symbols_7_thumbnail_square_0000.png");
+						slot2 = createLabelImage("video_slot_machine_symbols_cherries_thumbnail_square_0000.png");
+						slot3 =  createLabelImage("video_slot_machine_symbols_lemon_thumbnail_square_0000.png");
+		}catch(MalformedURLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
 		}
 		
-		pane.add(seven);
+		pane.add(slot1);
+		pane.add(slot2);
+		pane.add(slot3);
+		pane.add(spin);
+		
+		spin.addActionListener(this);
 		frame.pack();
 		frame.setVisible(true);
 		
@@ -45,5 +60,58 @@ public class SlotMachine {
 	JLabel imageLabel = new JLabel(icon);
 	return imageLabel;
 }
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		spin();
+	}
+	public void spin() {
+		int num1;
+		int num2;
+		int num3;
+		pane.removeAll();
+	
+		
+		JLabel current;
+		try {
+			for(int i =0;i<3;i++) {
+				int ran = (int)(Math.random()*3);	
+				if(i==0) {
+					current = slot1;
+					num1=ran;
+				}else if(i==1) {
+					current = slot2;
+					num2=ran;
+				}else {
+					current = slot3;
+					num3=ran;
+				}
+					
+				
+				if(ran==0) {
+						current = createLabelImage("video_slot_machine_symbols_7_thumbnail_square_0000.png");
+					
+				}else if(ran==1) {
+						current = createLabelImage("video_slot_machine_symbols_cherries_thumbnail_square_0000.png");
+					
+				}else{
+						current =  createLabelImage("video_slot_machine_symbols_lemon_thumbnail_square_0000.png");
+				}
+				pane.add(current);
+			}
+		}catch(MalformedURLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+		}
+			
+		//pane.add(slot1);
+		//pane.add(slot2);
+		//pane.add(slot3);
+		//if(num1==num2&&num2==num3) {
+			//ON THIS STEP
+		//}
+		pane.add(spin);
+		frame.pack();
+	}
 	
 }
